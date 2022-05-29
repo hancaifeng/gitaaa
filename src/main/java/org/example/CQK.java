cbfbdee5bf588b3fa0461d33f6c75e993c4057f9
cqk <cqk@732524448.com>
2022-05-29 14:24:14 +0800

    master第1次

---
+++ src/main/java/org/example/CQK.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class CQK {
+    public static void main(String[] args) {
+        System.out.println("我是1");
+        System.out.println("我是2");
+        System.out.println("我是3");
+    }
+}
